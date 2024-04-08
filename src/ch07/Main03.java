a311ddadffedd4f6464b9a0253a12802a2341056
JuwonKim2022 <popolily12@gmail.com>
2024-04-09 08:08:05 +0900

    2024-04-09 algorithm

---
+++ src/ch07/Main03.java
@@ -0,0 +1,18 @@
+package ch07;
+
+/*
+ * 팩토리얼
+ * - 자연수 N 입력되면 N! 구하는 프로그램 작성
+ */
+
+public class Main03 {
+    public int DFS(int n) {
+        if(n==1) return 1;
+        else return n*DFS(n-1);
+    }
+
+    public static void main(String[] args) {
+        Main03 T = new Main03();
+        System.out.println(T.DFS(5));
+    }
+}
